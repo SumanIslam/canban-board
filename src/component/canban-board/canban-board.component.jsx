2d1db3b2ff1b1b2d80259028c0c09909b2b5f448
Suman Islam <sumanislam1416@gmail.com>
2022-09-14 12:11:30 +0600

    create canban-board component and add styles to it

---
+++ src/component/canban-board/canban-board.component.jsx
@@ -0,0 +1,14 @@
+import react from 'react';
+
+// styles
+import '../../styles/style.css'
+
+const CanbanBoard = () => {
+  return (
+		<div>
+			<h1 class='main-title'>Kanban Board</h1>
+		</div>
+	);
+}
+
+export default CanbanBoard;
